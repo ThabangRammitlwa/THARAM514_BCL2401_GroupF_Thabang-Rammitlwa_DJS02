d835e5140a2257a4da701f9183af6beb7eeb8bbd
Thabang Rammitlwa <thabang451@gmail.com>
2024-05-09 14:13:52 +0200

    Updated code

--- scripts.js
+++ scripts.js
@@ -5,5 +5,25 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+  if (dividend === "" || divider === "") {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again.";
+    return;
+  }
+
+  if (parseInt(divider) === 0) {
+    result.classList.add("error-message");
+    result.innerText = "Division not performed. Invalid number provided. Try again";
+    console.error("Error: Division by zero");
+    return;
+  }
+  if (isNaN(parseInt(dividend)) || isNaN(parseInt(divider))) {
+    result.classList.add("critical-error");
+    result.innerText = "Something critical went wrong. Please reload the page";
+    console.error("Error: Invalid input provided");
+    return;
+  }
+
+  const quotient = Math.floor((dividend) / (divider));
+  result.innerText = `${quotient}`;
 });
